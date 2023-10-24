33dd5be011894099868b562fb1f1d9f91802fc19
Louka <louka.cipolat@yahoo.fr>
2023-10-24 16:51:05 +0100

    authentification(token) pour acces route

--- routes/stuff.js
+++ routes/stuff.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const stuffCrtl = require("../controllers/stuff");
 const auth = require('../middleware/auth');
 
-router.post('/', stuffCrtl.createThing);
-router.delete('/:id', stuffCrtl.deleteThing);
+router.post('/', auth, stuffCrtl.createThing);
+router.delete('/:id', auth, stuffCrtl.deleteThing);
 router.get('/', stuffCrtl.getAllStuff);
 router.get('/:id', stuffCrtl.getOneThing);
-router.put('/:id', stuffCrtl.modifyThing);
+router.put('/:id', auth, stuffCrtl.modifyThing);
 
 
 module.exports = router;
